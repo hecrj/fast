4242dba197b284d3dec786bb8df6cadd1e768c91
Héctor <hector0193@gmail.com>
2014-03-06 13:30:15 +0100

    Add coding declaration.

--- reticular.py
+++ reticular.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """
 Reticular is a lightweight Python module that can be used to create powerful command-line tools.
 It lets you define commands easily, without losing flexibility and control.
